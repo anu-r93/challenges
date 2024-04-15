b2897d47086bbf556f11ba386f8aed5ffe88ff99
Anushka Raman <anushkaraman93@gmail.com>
2024-04-15 10:51:43 +0200

    with some minor changes

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -8,19 +8,19 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
 
-pizzaInput1.addEventListener("input", (element) => {
+pizzaInput1.addEventListener("input", () => {
   // write your code here
-  const pizzaSize1 = element.target.value;
+  const pizzaSize1 = pizzaInput1.value;
   const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updatePizzaDisplay(pizza1, pizzaSize1);
   updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
-pizzaInput2.addEventListener("input", (element) => {
+pizzaInput2.addEventListener("input", () => {
   // write your code here
   const pizzaSize1 = pizzaInput1.value;
-  const pizzaSize2 = element.target.value;
+  const pizzaSize2 = pizzaInput2.value;
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updatePizzaDisplay(pizza2, pizzaSize2);
   updateOutputColor(pizzaSize1, pizzaSize2);
