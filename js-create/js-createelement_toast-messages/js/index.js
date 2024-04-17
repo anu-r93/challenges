3540fb6a31ce86015b32481500c4d019fe792670
Anushka Raman <anushkaraman93@gmail.com>
2024-04-17 12:02:31 +0200

    changed to roast message

--- js-create/js-createelement_toast-messages/js/index.js
+++ js-create/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,16 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const toast = document.createElement("li");
+  toast.textContent = "I am a roast message";
+  toast.classList.add("toast-container__message");
+  toastContainer.append(toast);
+
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
+
   // Exercise: Clear the stack of toast messages
 });
