b575dcfe1b0db952a9f6aae4c637cabc1154142c
Anushka Raman <anushkaraman93@gmail.com>
2024-04-26 11:40:11 +0200

    added fetch

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,12 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
 
 fetchData();
