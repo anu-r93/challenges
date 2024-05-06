3e911b14da45681e8ffd1ad504d37fcf3799cfa0
Anushka Raman <anushkaraman93@gmail.com>
2024-05-06 15:49:35 +0200

    added nested element

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Hello</Button>
+      <Button>What's Up!</Button>
+      <Button>All Good?</Button>
+      <Button>Have Fun</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
