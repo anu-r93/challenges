e8812ce4bbf0159f9ded2a1fcfaf57a04bfd5582
Anushka Raman <anushkaraman93@gmail.com>
2024-05-10 17:49:37 +0200

    added usestate and event handler

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import "./App.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+  const handleAdd = () => {
+    setPeople(people + 1);
+  };
+  const handleDelete = () => {
+    setPeople(people - 1);
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onClickAdd={handleAdd} onClickDelete={handleDelete} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
